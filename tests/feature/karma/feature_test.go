caf95109bf9afc82273add29b3437fff2dac7072
Ryan Van Antwerp <rvanantwerp@gmail.com>
2017-11-26 21:14:39 -0500

    add karma feature and tests

---
+++ tests/feature/karma/feature_test.go
@@ -0,0 +1,44 @@
+package karma
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/jakevoytko/crbot/app"
+	"github.com/jakevoytko/crbot/feature/karma"
+	"github.com/jakevoytko/crbot/testutil"
+)
+
+func TestKarma(t *testing.T) {
+	runner := testutil.NewRunner(t)
+
+	// Correct call format
+	runner.SendMessage(testutil.MainChannelID, "?++ target", fmt.Sprintf(karma.MsgIncrementKarma, "target", "target", 1))
+	runner.SendMessage(testutil.MainChannelID, "?-- target", fmt.Sprintf(karma.MsgDecrementKarma, "target", "target", 0))
+
+	// Can't give karma in private message
+	runner.SendMessage(testutil.DirectMessageID, "?++ target", fmt.Sprintf(app.MsgPublicOnly, "?++"))
+
+	// Wrong call format
+	runner.SendMessage(testutil.MainChannelID, "?++", karma.MsgHelpKarmaIncrement)
+	runner.SendMessage(testutil.MainChannelID, "?--", karma.MsgHelpKarmaDecrement)
+	runner.SendMessage(testutil.MainChannelID, "?++ ?call response", karma.MsgHelpKarmaIncrement)
+	runner.SendMessage(testutil.MainChannelID, "?-- ?call response", karma.MsgHelpKarmaDecrement)
+}
+
+func TestKarmaIncrement(t *testing.T) {
+	runner := testutil.NewRunner(t)
+
+	runner.SendMessage(testutil.MainChannelID, "?++ target", fmt.Sprintf(karma.MsgIncrementKarma, "target", "target", 1))
+	runner.SendMessage(testutil.MainChannelID, "?++ target", fmt.Sprintf(karma.MsgIncrementKarma, "target", "target", 2))
+	runner.SendMessage(testutil.MainChannelID, "?++ target", fmt.Sprintf(karma.MsgIncrementKarma, "target", "target", 3))
+}
+
+func TestKarmaDecrement(t *testing.T) {
+	runner := testutil.NewRunner(t)
+
+	runner.SendMessage(testutil.MainChannelID, "?-- target", fmt.Sprintf(karma.MsgDecrementKarma, "target", "target", -1))
+	runner.SendMessage(testutil.MainChannelID, "?-- target", fmt.Sprintf(karma.MsgDecrementKarma, "target", "target", -2))
+	runner.SendMessage(testutil.MainChannelID, "?-- target", fmt.Sprintf(karma.MsgDecrementKarma, "target", "target", -3))
+
+}
