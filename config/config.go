71581eeef65f0f424d5aa0d85466a7dc9c4194f1
Andrew E. Timmes <andrew.timmes@gmail.com>
2021-12-05 22:16:48 -0500

    add redis config params

--- config/config.go
+++ config/config.go
@@ -13,8 +13,21 @@ import (
 
 // Config represents the JSON format of the config file.
 type Config struct {
-	BotToken string            `json:"bot_token"`
-	RickList []model.Snowflake `json:"ricklist"`
+	BotToken      string            `json:"bot_token"`
+	RickList      []model.Snowflake `json:"ricklist"`
+	RedisHost     string            `json:"redis_host"`
+	RedisPort     int               `json:"redis_port"`
+	RedisUsername string            `json:"redis_username"`
+	RedisPassword string            `json:"redis_password"`
+	RedisDatabase int               `json:"redis_database"`
+}
+
+// SetDefaultConfig sets default values for config params that have them.
+func SetDefaultConfig(config *Config) {
+	config.RedisHost = "localhost"
+	config.RedisPort = 6379
+	config.RedisPassword = ""
+	config.RedisDatabase = 0
 }
 
 // ParseConfig reads the config from the given filename.
@@ -24,6 +37,7 @@ func ParseConfig(filename string) (*Config, error) {
 		return nil, e
 	}
 	var config Config
+	SetDefaultConfig(&config)
 	e = json.Unmarshal(f, &config)
 	return &config, e
 }
