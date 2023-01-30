8ec82d9359187c8e8a4285d08662bd7cf3cee33d
Andrew E. Timmes <andrew.timmes@gmail.com>
2023-01-30 11:11:51 -0500

    address PR comments

--- api/discordsession.go
+++ api/discordsession.go
@@ -5,7 +5,7 @@ import "github.com/bwmarrin/discordgo"
 // DiscordSession is an interface for interacting with Discord within a session
 // message handler.
 type DiscordSession interface {
-	ChannelMessageSend(channelID string, content string, options ...discordgo.RequestOption) (*discordgo.Message, error)
+	ChannelMessageSend(channelID, content string, options ...discordgo.RequestOption) (*discordgo.Message, error)
 	Channel(channelID string, options ...discordgo.RequestOption) (*discordgo.Channel, error)
 	User(userID string, options ...discordgo.RequestOption) (*discordgo.User, error)
 }
